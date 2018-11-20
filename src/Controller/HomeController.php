3f8c9cd0cd3255ed0649f7209c6b7dc43b20a914
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2018-11-20 17:01:47 +0100

    small code style fix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class HomeController
+ *
+ * @package App\Controller
+ */
 class HomeController extends AbstractController
 {
     /**
      * @Route("")
+     *
+     * @return Response
      */
-    public function index()
+    public function index(): Response
     {
         $number = random_int(0, 100);
 
-        return $this->render('home/index.html.twig', array('number' => $number));
+        return $this->render('home/index.html.twig', ['number' => $number]);
     }
 }
