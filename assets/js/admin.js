2f3f0bc804c71acf6d0cb7e1b3096152ce0a61ed
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2019-01-04 17:54:47 +0100

    experimenting with js image uploader

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,15 +1,26 @@
 import '@webcomponents/webcomponentsjs';
 import '@ungap/custom-elements-builtin';
+import Trix from 'trix';
+import moment from 'moment';
+import * as FilePond from 'filepond';
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import FilePondPluginImageResize from 'filepond-plugin-image-resize';
+import FilePondPluginImageTransform from 'filepond-plugin-image-transform';
+import FilePondPluginFileEncode from 'filepond-plugin-file-encode';
+
+import 'filepond/dist/filepond.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
 require('select2');
 $.fn.select2.defaults.set('theme', 'bootstrap4');
 
-const Trix = require('trix');
 Trix.config.attachments.preview.caption.name = false;
 Trix.config.attachments.preview.caption.size = false;
 Trix.config.attachments.file.caption.size = false;
 
-const moment = require('moment');
 global.moment = moment;
 require('tempusdominus-bootstrap-4');
 $.fn.datetimepicker.Constructor.Default = $.extend({}, $.fn.datetimepicker.Constructor.Default, {
@@ -31,12 +42,25 @@ $.fn.datetimepicker.Constructor.Default = $.extend({}, $.fn.datetimepicker.Const
     }
 });
 
+FilePond.setOptions({});
+FilePond.registerPlugin(
+    FilePondPluginImageExifOrientation,
+    FilePondPluginFileValidateType,
+    FilePondPluginFileValidateSize,
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginImageTransform,
+    FilePondPluginFileEncode
+);
+
 $(document).ready(function () {
-    $('[data-enable-select2="true"]').each(function () {
+    $('select[data-enable-select2="true"]').each(function () {
         $(this).select2($(this).data('select2-options'));
     });
 
     $('[data-enable-datepicker="true"]').each(function () {
         $(this).datetimepicker($(this).data('datepicker-options'));
     });
+
+    FilePond.parse(document.body);
 });
