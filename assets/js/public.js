ac14debae968cbfafb6627e5accc9d9c5d7b6629
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2019-01-05 11:30:32 +0100

    experiments with dropzone

--- assets/js/public.js
+++ assets/js/public.js
@@ -4,6 +4,7 @@ import 'time-elements';
 import './public/app-email';
 import Trix from 'trix';
 import moment from 'moment';
+import Dropzone from 'dropzone';
 
 import '../css/public.scss';
 import '@fortawesome/fontawesome-free/css/all.css';
@@ -19,6 +20,8 @@ $.fn.select2.defaults.set('theme', 'bootstrap4');
 Trix.config.attachments.preview.caption.name = false;
 Trix.config.attachments.preview.caption.size = false;
 Trix.config.attachments.file.caption.size = false;
+
+Dropzone.autoDiscover = false;
 
 global.moment = moment;
 require('tempusdominus-bootstrap-4');
@@ -45,10 +48,37 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
     $('select[data-enable-select2="true"]').each(function () {
-        $(this).select2($(this).data('select2-options'));
+        $(this).select2($(this).data('select2-options') || {});
     });
 
     $('[data-enable-datepicker="true"]').each(function () {
-        $(this).datetimepicker($(this).data('datepicker-options'));
+        $(this).datetimepicker($(this).data('datepicker-options') || {});
+    });
+
+    $('[data-enable-dropzone="true"]').each(function () {
+        const templateSelector = $(this).data('dropzone-template') || null;
+
+        const options = $.extend({
+            url: $(this).data('dropzone-url') || null,
+            init: function () {
+                this.on('error', (file, msg, xhr) => {
+                    if (!file.accepted) {
+                        this.removeFile(file);
+                    }
+                });
+                this.on('maxfilesexceeded', (file) => {
+                    this.removeFile(this.getQueuedFiles()[0]);
+                    this.emit("addedfile", file);
+                    this.emit("complete", file);
+                });
+            }
+        }, $(this).data('dropzone-options') || {});
+
+        if (templateSelector) {
+            options.previewTemplate = $(templateSelector).html();
+            $(templateSelector).remove();
+        }
+
+        $(this).dropzone(options);
     });
 });
