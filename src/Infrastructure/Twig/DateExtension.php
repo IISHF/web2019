9d2132adb42693057b52a0fb5c8026a59f7e89dc
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2019-01-08 15:38:32 +0100

    adds support for additional custom time elements

--- src/Infrastructure/Twig/DateExtension.php
+++ src/Infrastructure/Twig/DateExtension.php
@@ -21,24 +21,99 @@ class DateExtension extends \Twig_Extension
     public function getFilters(): array
     {
         return [
+            new \Twig_SimpleFilter('relative_time', [$this, 'formatRelativeTime'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFilter('time_until', [$this, 'formatTimeUntil'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFilter('time_ago', [$this, 'formatTimeAgo'], ['is_safe' => ['html']]),
             new \Twig_SimpleFilter(
-                'relative_date',
-                [$this, 'formatRelativeDate'],
-                ['is_safe' => ['html']]
+                'local_time',
+                [$this, 'formatLocalTime'],
+                ['is_safe' => ['html'], 'needs_environment' => true]
             ),
         ];
     }
 
     /**
      * @param \DateTimeInterface $dateTime
-     * @return string
+     * @return string[]
      */
-    public function formatRelativeDate(\DateTimeInterface $dateTime): string
+    private function createTimeStrings(\DateTimeInterface $dateTime): array
     {
         $isoDate      = $dateTime->format(\DateTimeInterface::W3C);
         $readableDate = $dateTime->format('F j, Y H:i');
+        return [$isoDate, $readableDate];
+    }
+
+    /**
+     * @param \DateTimeInterface $dateTime
+     * @return string
+     */
+    public function formatRelativeTime(\DateTimeInterface $dateTime): string
+    {
+        [$isoDate, $readableDate] = $this->createTimeStrings($dateTime);
         return <<<HTML
 <relative-time datetime="$isoDate" data-toggle="tooltip" data-placement="top">$readableDate</relative-time>
+HTML;
+    }
+
+    /**
+     * @param \DateTimeInterface $dateTime
+     * @return string
+     */
+    public function formatTimeUntil(\DateTimeInterface $dateTime): string
+    {
+        [$isoDate, $readableDate] = $this->createTimeStrings($dateTime);
+        return <<<HTML
+<time-until datetime="$isoDate" data-toggle="tooltip" data-placement="top">$readableDate</time-until>
+HTML;
+    }
+
+    /**
+     * @param \DateTimeInterface $dateTime
+     * @param bool               $micro
+     * @return string
+     */
+    public function formatTimeAgo(\DateTimeInterface $dateTime, bool $micro = false): string
+    {
+        [$isoDate, $readableDate] = $this->createTimeStrings($dateTime);
+        $microAttr = $micro ? 'format="micro"' : '';
+        return <<<HTML
+<time-ago datetime="$isoDate" $microAttr data-toggle="tooltip" data-placement="top">$readableDate</time-ago>
+HTML;
+    }
+
+    /**
+     * @param \Twig_Environment  $env
+     * @param \DateTimeInterface $dateTime
+     * @param array              $options
+     * @return string
+     */
+    public function formatLocalTime(\Twig_Environment $env, \DateTimeInterface $dateTime, array $options = []): string
+    {
+        $defaultOptions = [
+            'year'           => 'numeric',
+            'month'          => 'long',
+            'day'            => 'numeric',
+            'weekday'        => null,
+            'hour'           => 'numeric',
+            'minute'         => 'numeric',
+            'second'         => null,
+            'time-zone-name' => null,
+        ];
+        $options        = array_intersect_key($options, $defaultOptions);
+        $optionsAttrs   = [];
+        foreach (array_replace($defaultOptions, $options) as $k => $v) {
+            if ($v !== null) {
+                $optionsAttrs[] = \twig_escape_filter($env, $k)
+                    . '="'
+                    . \twig_escape_filter($env, $v, 'html_attr')
+                    . '"';
+            }
+        }
+        $optionsAttr = implode(' ', $optionsAttrs);
+
+        [$isoDate, $readableDate] = $this->createTimeStrings($dateTime);
+        return <<<HTML
+<local-time datetime="$isoDate" $optionsAttr data-toggle="tooltip" data-placement="top">$readableDate</local-time>
 HTML;
     }
 }
