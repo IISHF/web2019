b1b1750d5a0d6fa64941f7860cbf358fef6a174e
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2019-01-12 12:35:17 +0100

    fixes table name

--- src/Domain/Model/Event/TitleEventApplication.php
+++ src/Domain/Model/Event/TitleEventApplication.php
@@ -19,7 +19,7 @@ use Webmozart\Assert\Assert;
  * @package App\Domain\Model\Event
  *
  * @ORM\Entity()
- * @ORM\Table(name="event_applicant")
+ * @ORM\Table(name="event_applications")
  */
 class TitleEventApplication
 {
