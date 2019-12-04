da0d2b56bfccfeed1eda87d7eae545f25d3918f9
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2019-12-04 11:34:05 +0100

    Changelogs summary:
     - misd/phone-number-bundle removed (installed version was v1.3.2)
    
     - odolbeau/phone-number-bundle installed in version v3.0.0
       Release notes: https://github.com/odolbeau/phone-number-bundle/releases/tag/v3.0.0

--- config/bundles.php
+++ config/bundles.php
@@ -12,8 +12,8 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Misd\PhoneNumberBundle\MisdPhoneNumberBundle::class => ['all' => true],
     WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
     Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Misd\PhoneNumberBundle\MisdPhoneNumberBundle::class => ['all' => true],
 ];
