a3182b0f594ca29a4ff9fee62ff03de034aa2e0b
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2020-12-29 10:49:12 +0100

    updates symfony/webpack-encore-bundle recipe

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const Encore = require('@symfony/webpack-encore');
+var Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
