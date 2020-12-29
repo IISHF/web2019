488a25734f6d72545585353a6295d0633170208b
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2020-12-29 10:36:48 +0100

    updates symfony/phpunit-bridge recipe

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,53 +1,11 @@
 <?php
 
-use App\Kernel;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
-    require dirname(__DIR__) . '/config/bootstrap.php';
+if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
+    require dirname(__DIR__).'/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
-}
-
-function bootstrap()
-{
-    $kernel = new Kernel('test', true);
-    $kernel->boot();
-
-    $application = new Application($kernel);
-    $application->setAutoExit(false);
-    $application->run(
-        new ArrayInput(
-            [
-                'command'     => 'doctrine:database:drop',
-                '--if-exists' => '1',
-                '--force'     => '1',
-            ]
-        )
-    );
-    $application->run(
-        new ArrayInput(
-            [
-                'command' => 'doctrine:database:create',
-            ]
-        )
-    );
-    $application->run(
-        new ArrayInput(
-            [
-                'command'              => 'doctrine:migrations:migrate',
-                '--allow-no-migration' => '1',
-                '--no-interaction'     => '1',
-            ]
-        )
-    );
-    $kernel->shutdown();
-}
-
-if (($_SERVER['APP_ENV'] ?? '') === 'test' && ($_SERVER['DISABLE_DB_SETUP'] ?? '0') === '0') {
-    bootstrap();
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
