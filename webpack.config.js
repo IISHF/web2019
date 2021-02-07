49b99f27827b2750e7fbc493d65ad68dbb9e6b0f
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2021-02-07 15:02:25 +0100

    updates symfony/webpack-encore-bundle recipe

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -7,51 +7,85 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 }
 
 Encore
+    // directory where compiled assets will be stored
     .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
     .setPublicPath('/build')
-    .setManifestKeyPrefix('build/')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
 
+    /*
+     * ENTRY CONFIG
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('public', './assets/js/public.js')
+    .addEntry('login', './assets/js/login.js')
+    .addEntry('admin', './assets/js/admin.js')
+
+    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    //.enableStimulusBridge('./assets/controllers.json')
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+    /*
+     * FEATURE CONFIG
+     *
+     * Enable & configure other features below. For a full
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
-    .enableIntegrityHashes(Encore.isProduction())
 
+    .configureBabel((config) => {
+        if (!Encore.isProduction()) {
+                config.plugins.push('react-hot-loader/babel');
+            }
+            config.plugins.push('@babel/plugin-proposal-class-properties');
+            config.plugins.push('@babel/plugin-transform-runtime');
+    })
+
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
+    // enables Sass/SCSS support
     .enableSassLoader()
     .enablePostCssLoader((options) => {
         options.postcssOptions = {
             config: 'assets/postcss.config.js'
         };
     })
+
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
+
+    // uncomment if you use React
     .enableReactPreset()
 
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    .enableIntegrityHashes(Encore.isProduction())
+
+    // uncomment if you're having problems with a jQuery plugin
     .autoProvidejQuery()
 
-    .addEntry('public', './assets/js/public.js')
-    .addEntry('login', './assets/js/login.js')
-    .addEntry('admin', './assets/js/admin.js')
     .copyFiles({
         from: './assets/img',
         to: 'images/[path][name].[hash:8].[ext]',
     })
-
-    .splitEntryChunks()
-    .enableSingleRuntimeChunk()
-
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-
-    .configureBabel(
-        (babelConfig) => {
-            if (!Encore.isProduction()) {
-                babelConfig.plugins.push('react-hot-loader/babel');
-            }
-            babelConfig.plugins.push('@babel/plugin-proposal-class-properties');
-            babelConfig.plugins.push('@babel/plugin-transform-runtime');
-        },
-        {
-            useBuiltIns: 'usage',
-            corejs: 3
-        }
-    )
 
     .configureDevServerOptions(options => {
         options.https = {
