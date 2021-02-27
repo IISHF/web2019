0d56a979917c5f690486ec3ed8708df44d47ec75
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2021-02-27 08:00:11 +0100

    small update to the upgrade script

--- deployment/upgrade.sh
+++ deployment/upgrade.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd -P`
-popd > /dev/null
+pushd $(dirname $0) >/dev/null
+SCRIPTPATH=$(pwd -P)
+popd >/dev/null
 
 cd ${SCRIPTPATH}/../
 
-/usr/local/bin/composer install --prefer-dist --optimize-autoloader --apcu-autoloader
+/usr/local/bin/composer install --no-dev --prefer-dist --optimize-autoloader --apcu-autoloader
 APP_ENV=dev ./bin/console cache:clear --no-warmup
 APP_ENV=dev ./bin/console doctrine:migrations:migrate --no-interaction
+rm -rf ./var/cache/*
 APP_ENV=prod ./bin/console cache:clear --no-warmup
 APP_ENV=prod ./bin/console cache:warmup
 
