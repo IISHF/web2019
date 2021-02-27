1ee080f44c1a59dd1e96eacc098ec95a5edf0fc9
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2021-02-27 09:45:14 +0100

    dev is currently required for prod

--- deployment/upgrade.sh
+++ deployment/upgrade.sh
@@ -6,7 +6,7 @@ popd >/dev/null
 
 cd ${SCRIPTPATH}/../
 
-/usr/local/bin/composer install --no-dev --prefer-dist --optimize-autoloader --apcu-autoloader
+/usr/local/bin/composer install --prefer-dist --optimize-autoloader --apcu-autoloader
 APP_ENV=dev ./bin/console cache:clear --no-warmup
 APP_ENV=dev ./bin/console doctrine:migrations:migrate --no-interaction
 rm -rf ./var/cache/*
